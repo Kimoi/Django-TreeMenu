1b103647a416bcb62d6581ab0901e96d5a8912b6
Mark_Dolgov <hontonikimoi@gmail.com>
2024-02-15 00:53:37 +0300

    add models, migrations, admin register

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return self.name
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    url = models.URLField(blank=True)  # default length of 200
+    parent = models.ForeignKey(
+        'self', on_delete=models.CASCADE, null=True, blank=True)
+    menu = models.ForeignKey(
+        Menu, on_delete=models.CASCADE, related_name='menu_items')
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return self.name
